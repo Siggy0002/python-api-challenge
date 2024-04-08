073df4a435597ba7c9ed8352300a65ae8e67f478
Andrew Giannettino <andrewgiannettino@Andrews-MacBook-Air-2.local>
2024-04-08 18:09:53 -0500

    Completed part 1 A of weather py

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8d034f7b64e67cf76f911492b02f45c9"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "8a21f73673ff477abb86ac98de693312"
